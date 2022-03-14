3d0eae1e3a7c2c1c98c83d4416a81e98f083e5c9
lai2-01 <lai2-01@itismeucci.com>
2022-03-14 12:44:13 +0100

    primo commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i = 0 ; i < 53687106677; i++ )
+    {  
+        System.out.println("Hello, World!");
+    }
+    }
+}
